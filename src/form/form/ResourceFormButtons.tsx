be75ccdc6829e4fe57f69ec131eed43ad32e8e9a
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2018-10-31 07:27:34 +0100

    Update ResourceFormButtons

--- src/form/form/ResourceFormButtons.tsx
+++ src/form/form/ResourceFormButtons.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 import { FormContext } from './Form';
 
-import { Button, Popconfirm, Form } from 'antd';
+import { Button, Popconfirm, Form, Menu, Dropdown } from 'antd';
+import { ClickParam } from 'antd/lib/menu';
 
 export interface ResourceFormButtonsProps {
   formContext: FormContext;
@@ -14,6 +15,18 @@ export class ResourceFormButtons extends React.Component<
   render() {
     const { formContext } = this.props;
     const hasChanges = formContext.form.isFieldsTouched();
+
+    const saveMenu = (
+      <Menu
+        onClick={(param: ClickParam) => {
+          global.console.log(param.item);
+        }}
+      >
+        <Menu.Item key="1">Save and add new</Menu.Item>
+        <Menu.Item key="2">Save and continue editing</Menu.Item>
+      </Menu>
+    );
+
     return (
       <Form.Item
         wrapperCol={{
@@ -21,9 +34,19 @@ export class ResourceFormButtons extends React.Component<
           sm: { span: 16, offset: 8 }
         }}
       >
-        <Button disabled={!hasChanges} type="primary" htmlType="submit">
-          Send
-        </Button>
+        <Dropdown.Button
+          disabled={!hasChanges}
+          type="primary"
+          overlay={saveMenu}
+          onClick={() => {
+            formContext.formComponent.submit();
+          }}
+        >
+          Save
+        </Dropdown.Button>
+        {/* <Button disabled={!hasChanges} type="primary" htmlType="submit">
+          Save
+        </Button> */}
         <Popconfirm
           title="Reset?"
           cancelText="No"
