161b324572be8ce753bfba395032fabcac507c98
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-01-04 10:01:29 +0100

    wip

--- src/table/ResourceTable.tsx
+++ src/table/ResourceTable.tsx
@@ -163,6 +163,8 @@ export class ResourceTable extends React.Component<
           sortedInfo && c.key === sortedInfo.columnKey
             ? sortedInfo.order
             : undefined;
+      } else {
+        c.sortOrder = undefined;
       }
 
       return c;
