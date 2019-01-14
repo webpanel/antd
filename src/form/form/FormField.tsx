cc4b1d932ac336683e8de753deb6e13c69bca621
Jakub Knejzlik <jakub.knejzlik@inloop.eu>
2019-01-14 12:39:59 +0100

    Disable undefined values normalizati

--- src/form/form/FormField.tsx
+++ src/form/form/FormField.tsx
@@ -37,12 +37,6 @@ export class FormField extends React.Component<FormFieldProps> {
     let props = Object.assign({}, { ...this.props });
 
     props.initialValue = props.initialValue || get(initialValues, namePath);
-    props.normalize = (value: any, prevValue: any, allValues: any) => {
-      if (typeof value === 'undefined') {
-        return null;
-      }
-      return value;
-    };
 
     let children = Array.isArray(this.props.children)
       ? this.props.children
