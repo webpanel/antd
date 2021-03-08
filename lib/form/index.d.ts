1e919e7c13e0cba9920b75aaf4e67129a86cb4e0
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-03-08 14:16:58 +0100

    Remove unused reference

--- lib/form/index.d.ts
+++ lib/form/index.d.ts
@@ -1,4 +1,3 @@
 export { ResourceForm } from "./ResourceForm";
 export { ResourceFormButtons } from "./ResourceFormButtons";
-export { default as FormItem } from "antd/lib/form/FormItem";
 export { Input } from "./Input";
